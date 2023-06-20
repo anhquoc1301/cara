072e59ce3df6153172e3c75e39cc1f1bc5e21996
anh nguyen <anhquoc1301@gmail.com>
2023-06-20 13:23:25 +0700

    fix setting cron

--- TradeBTC/settings.py
+++ TradeBTC/settings.py
@@ -150,6 +150,5 @@ MESSAGE_TAGS = {
 }
 
 CRONJOBS = [
-    ('*/1 * * * *', 'ModuleApp.cron.my_scheduled_job_phase')
-    # ('0 2 * * *', 'ModuleApp.cron.my_scheduled_job_refresh_guest')
+    ('*/3 * * * *', 'ModuleApp.cron.my_scheduled_job_phase')
 ]
